e7c93e057466bb4ce23b1b2e0645dc8e792e1ea2
Rajan Khullar <rkhullar@nyit.edu>
2016-10-10 19:25:39 -0400

    dev

--- target.c
+++ target.c
@@ -14,14 +14,19 @@ int enc(const char *path, const char *key, const char *out)
     fk = fopen(key , "r"); if(fk == NULL) return -1; ck = 0;
     fi = fopen(path, "r"); if(fi == NULL) return -1; ci = 0;
     fo = fopen(out , "w"); if(fo == NULL) return -1; co = 0;
-    while(ci != EOF && ck != EOF)
+    unsigned int n = 0;
+    while(true)
     {
         ci = fgetc(fi);
         ck = fgetc(fk);
+        if(ci == EOF || ck == EOF)
+            break;
         co = ci^ck;
         fputc(co, fo);
+        n++;
     }
     fclose(fi); fclose(fk); fclose(fo);
+    printf("%d\n", n);
     return 0;
 }
 
